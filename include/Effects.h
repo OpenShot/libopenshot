e4d6c026b7240e9f3ddc814e68258a71b5f4742b
Jonathan Thomas <Jonathan.Oomph@gmail.com>
2014-01-10 17:24:40 -0600

    Forgot to add the new Effects header file. Here it is.

---
+++ include/Effects.h
@@ -0,0 +1,38 @@
+#ifndef OPENSHOT_EFFECTS_H
+#define OPENSHOT_EFFECTS_H
+
+/**
+ * @file
+ * @brief This header includes all commonly used effects for libopenshot, for ease-of-use.
+ * @author Jonathan Thomas <jonathan@openshot.org>
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2008-2013 OpenShot Studios, LLC
+ * (http://www.openshotstudios.com). This file is part of
+ * OpenShot Library (http://www.openshot.org), an open-source project
+ * dedicated to delivering high quality video editing and animation solutions
+ * to the world.
+ *
+ * OpenShot Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenShot Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Effects */
+#include "effects/ChromaKey.h"
+#include "effects/Deinterlace.h"
+#include "effects/Mask.h"
+#include "effects/Negate.h"
+
+
+#endif
