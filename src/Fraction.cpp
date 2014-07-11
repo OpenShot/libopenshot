e135eb5738f7ad3d00390d99f2e73dad8f404bd0
Jonathan Thomas <jonathan@openshot.org>
2014-07-11 16:52:14 -0500

    Changed license to LGPLv3 for all libopenshot code.

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -12,30 +12,17 @@
  * world. For more information visit <http://www.openshot.org/>.
  *
  * OpenShot Library (libopenshot) is free software: you can redistribute it
- * and/or modify it under the terms of the GNU Affero General Public License
+ * and/or modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * OpenShot Library (libopenshot) is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with OpenShot Library. If not, see <http://www.gnu.org/licenses/>.
- *
- * Also, if your software can interact with users remotely through a computer
- * network, you should also make sure that it provides a way for users to
- * get its source. For example, if your program is a web application, its
- * interface could display a "Source" link that leads users to an archive
- * of the code. There are many ways you could offer source, and different
- * solutions will be better for different programs; see section 13 for the
- * specific requirements.
- *
- * You should also get your employer (if you work as a programmer) or school,
- * if any, to sign a "copyright disclaimer" for the program, if necessary.
- * For more information on this, and how to apply and follow the GNU AGPL, see
- * <http://www.gnu.org/licenses/>.
  */
 
 #include "../include/Fraction.h"
