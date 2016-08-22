82d3798a1808a72625a49c37540223e19a2cbd0d
Jonathan Thomas <jonathan@openshot.org>
2016-08-21 21:06:50 -0500

    Fixing JSON property value ranges on mask effect

--- src/effects/Mask.cpp
+++ src/effects/Mask.cpp
@@ -295,7 +295,7 @@ string Mask::PropertiesJSON(long int requested_frame) {
 	root["replace_image"]["choices"].append(add_property_choice_json("No", false, replace_image));
 
 	// Keyframes
-	root["brightness"] = add_property_json("Brightness", brightness.GetValue(requested_frame), "float", "", brightness.Contains(requested_point), brightness.GetCount(), -100, 100, brightness.GetClosestPoint(requested_point).interpolation, brightness.GetClosestPoint(requested_point).co.X, false);
+	root["brightness"] = add_property_json("Brightness", brightness.GetValue(requested_frame), "float", "", brightness.Contains(requested_point), brightness.GetCount(), -1.0, 1.0, brightness.GetClosestPoint(requested_point).interpolation, brightness.GetClosestPoint(requested_point).co.X, false);
 	root["contrast"] = add_property_json("Contrast", contrast.GetValue(requested_frame), "float", "", contrast.Contains(requested_point), contrast.GetCount(), 0, 20, contrast.GetClosestPoint(requested_point).interpolation, contrast.GetClosestPoint(requested_point).co.X, false);
 
 	// Return formatted string
