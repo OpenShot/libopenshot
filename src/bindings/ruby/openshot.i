OpenShot/libopenshot

commit f12ffe692b08613bcfeeb2d36a3aaace49662004

Migrating tr1 to std, adding C++11 support to build scripts, fixing crash in FFmpegReader (caused by shared_ptr, buffer, and last_video_frame corruption). Much improved stability with this change. Thanks to Craig and Peter for the help!

src/bindings/ruby/openshot.i
@@ -40,7 +40,6 @@
 %include <std_except.i>
 
 namespace std {
-namespace tr1
 {
   template<class T> class shared_ptr {
   public:
@@ -50,11 +49,11 @@ namespace tr1
 
 /* Mark these classes as shared_ptr classes */
 #ifdef USE_IMAGEMAGICK
-	%template(SPtrImage) std::tr1::shared_ptr<Magick::Image>;
+	%template(SPtrImage) std::shared_ptr<Magick::Image>;
 #endif
-%template(SPtrAudioBuffer) std::tr1::shared_ptr<juce::AudioSampleBuffer>;
-%template(SPtrOpenFrame) std::tr1::shared_ptr<openshot::Frame>;
-%template(SPtrFrame) std::tr1::shared_ptr<Frame>;
+%template(SPtrAudioBuffer) std::shared_ptr<juce::AudioSampleBuffer>;
+%template(SPtrOpenFrame) std::shared_ptr<openshot::Frame>;
+%template(SPtrFrame) std::shared_ptr<Frame>;
 
 
 %{

src/bindings/python/openshot.i
@@ -40,7 +40,6 @@
 %include <std_except.i>
 
 /* Include shared pointer code */
-#define SWIG_SHARED_PTR_SUBNAMESPACE tr1
 %include <std_shared_ptr.i>
 
 /* Mark these classes as shared_ptr classes */

commit e5caeaa63e03975b9b9c9751918607e710d40582

Fix regression caused by f12ffe69

src/bindings/ruby/openshot.i
@@ -40,12 +40,11 @@
 %include <std_except.i>
 
 namespace std {
-{
   template<class T> class shared_ptr {
   public:
     T *operator->();
   };
-}}
+}
 
 /* Mark these classes as shared_ptr classes */
 #ifdef USE_IMAGEMAGICK