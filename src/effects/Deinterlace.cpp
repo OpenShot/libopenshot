cb6ac2121901e8d47c10a850ab7c7cb358b0a1d0
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-07-03 06:29:32 -0400

    src/effects: Catch-by-reference

--- src/effects/Deinterlace.cpp
+++ src/effects/Deinterlace.cpp
@@ -134,7 +134,7 @@ void Deinterlace::SetJson(string value) {
 		// Set all values that match
 		SetJsonValue(root);
 	}
-	catch (exception e)
+	catch (const std::exception& e)
 	{
 		// Error parsing JSON (or missing keys)
 		throw InvalidJSON("JSON is invalid (missing keys or invalid data types)", "");
--- src/effects/Saturation.cpp
+++ src/effects/Saturation.cpp
@@ -152,7 +152,7 @@ void Saturation::SetJson(string value) {
 		// Set all values that match
 		SetJsonValue(root);
 	}
-	catch (exception e)
+	catch (const std::exception& e)
 	{
 		// Error parsing JSON (or missing keys)
 		throw InvalidJSON("JSON is invalid (missing keys or invalid data types)", "");
