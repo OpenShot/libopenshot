e1ffe0762ff01090457284252c1d0078e96b99f4
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-07-03 15:24:45 -0400

    ZmqLogger.h: Correct default values for optional params

--- include/ZmqLogger.h
+++ include/ZmqLogger.h
@@ -95,12 +95,12 @@ namespace openshot {
 
 		/// Append debug information
 		void AppendDebugMethod(std::string method_name,
-					std::string arg1_name="", float arg1_value=-0.0,
-					std::string arg2_name="", float arg2_value=-0.0,
-					std::string arg3_name="", float arg3_value=-0.0,
-					std::string arg4_name="", float arg4_value=-0.0,
-					std::string arg5_name="", float arg5_value=-0.0,
-					std::string arg6_name="", float arg6_value=-0.0);
+					std::string arg1_name="", float arg1_value=-1.0,
+					std::string arg2_name="", float arg2_value=-1.0,
+					std::string arg3_name="", float arg3_value=-1.0,
+					std::string arg4_name="", float arg4_value=-1.0,
+					std::string arg5_name="", float arg5_value=-1.0,
+					std::string arg6_name="", float arg6_value=-1.0);
 
 		/// Close logger (sockets and/or files)
 		void Close();
