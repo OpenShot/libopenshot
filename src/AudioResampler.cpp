066e2558eee55c8b48ed3ed0e8e055f8e61eb3bf
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-08-05 03:52:15 -0400

    AudioResampler: Add juce:: prefixing

--- src/AudioResampler.cpp
+++ src/AudioResampler.cpp
@@ -74,7 +74,7 @@ AudioResampler::~AudioResampler()
 }
 
 // Sets the audio buffer and updates the key settings
-void AudioResampler::SetBuffer(AudioSampleBuffer *new_buffer, double sample_rate, double new_sample_rate)
+void AudioResampler::SetBuffer(juce::AudioSampleBuffer *new_buffer, double sample_rate, double new_sample_rate)
 {
 	if (sample_rate <= 0)
 		sample_rate = 44100;
@@ -89,7 +89,7 @@ void AudioResampler::SetBuffer(AudioSampleBuffer *new_buffer, double sample_rate
 }
 
 // Sets the audio buffer and key settings
-void AudioResampler::SetBuffer(AudioSampleBuffer *new_buffer, double ratio)
+void AudioResampler::SetBuffer(juce::AudioSampleBuffer *new_buffer, double ratio)
 {
 	// Update buffer & buffer source
 	buffer = new_buffer;
@@ -120,7 +120,7 @@ void AudioResampler::SetBuffer(AudioSampleBuffer *new_buffer, double ratio)
 }
 
 // Get the resampled audio buffer
-AudioSampleBuffer* AudioResampler::GetResampledBuffer()
+juce::AudioSampleBuffer* AudioResampler::GetResampledBuffer()
 {
 	// Resample the current frame's audio buffer (into the temp callback buffer)
 	resample_source->getNextAudioBlock(resample_callback_buffer);
