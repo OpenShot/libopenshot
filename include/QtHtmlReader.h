b90a83dd4c7b9e747f40307ed75c7de65ce70f1d
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-08-12 09:39:26 -0400

    Fix documentation-comment formatting, for Doxygen

--- include/QtHtmlReader.h
+++ include/QtHtmlReader.h
@@ -3,7 +3,10 @@
  * @brief Header file for QtHtmlReader class
  * @author Jonathan Thomas <jonathan@openshot.org>
  *
- * @section LICENSE
+ * @ref License
+ */
+
+/* LICENSE
  *
  * Copyright (c) 2008-2019 OpenShot Studios, LLC
  * <http://www.openshotstudios.com/>. This file is part of
