e15c0c168be3f8c7cff3e175b2123f8193a20d63
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-10-25 13:42:57 -0400

    Set still-image duration to 1 hour

--- src/QtImageReader.cpp
+++ src/QtImageReader.cpp
@@ -123,7 +123,7 @@ void QtImageReader::Open()
 		info.height = image->height();
 		info.pixel_ratio.num = 1;
 		info.pixel_ratio.den = 1;
-		info.duration = 1;
+		info.duration = 60 * 60 * 1;  // 1 hour duration
 		info.fps.num = 30;
 		info.fps.den = 1;
 		info.video_timebase.num = 1;
