ccc31c112e73012888ffaafe743801d260070586
Brad Kartchner <kartchnb@gmail.com>
2020-04-03 14:17:30 -0600

    Rethought my changes to KeyFrame
    I made my changes to InterpolateBetween in KeyFrame.cpp less intrusive.

--- src/KeyFrame.cpp
+++ src/KeyFrame.cpp
@@ -87,17 +87,15 @@ namespace {
 
 	double InterpolateBetween(Point const & left, Point const & right, double target, double allowed_error) {
 		assert(left.co.X < target);
-		assert(target <= right.co.X);
-        
-        double returnValue = 0.00;
-        
+		assert(target <= right.co.X);        
 		switch (right.interpolation) {
-		case CONSTANT: returnValue = left.co.Y;
-		case LINEAR: returnValue = InterpolateLinearCurve(left, right, target);
-		case BEZIER: returnValue = InterpolateBezierCurve(left, right, target, allowed_error);
+		case CONSTANT: return left.co.Y;
+		case LINEAR: return InterpolateLinearCurve(left, right, target);
+		case BEZIER: return InterpolateBezierCurve(left, right, target, allowed_error);
 		}
         
-        return returnValue;
+        // Control should never reach this point
+        return 0.0;
 	}
 
 
