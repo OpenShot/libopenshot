c14289a331f06bd85df1ae331f331b48b717791f
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-04-22 02:26:00 -0400

    PlayerPrivate: Fix indentation

--- src/Qt/PlayerPrivate.cpp
+++ src/Qt/PlayerPrivate.cpp
@@ -57,17 +57,17 @@ namespace openshot
     // Start thread
     void PlayerPrivate::run()
     {
-		// bail if no reader set
-		if (!reader)
-			return;
+        // bail if no reader set
+        if (!reader)
+            return;
 
-		// Start the threads
-		if (reader->info.has_audio)
-			audioPlayback->startThread(8);
-		if (reader->info.has_video) {
-			videoCache->startThread(2);
-			videoPlayback->startThread(4);
-		}
+        // Start the threads
+        if (reader->info.has_audio)
+            audioPlayback->startThread(8);
+        if (reader->info.has_video) {
+            videoCache->startThread(2);
+            videoPlayback->startThread(4);
+        }
 
         using std::chrono::duration_cast;
 
@@ -78,40 +78,40 @@ namespace openshot
         // Calculate on-screen time for a single frame in milliseconds
         const auto frame_duration = double_ms(1000.0 / reader->info.fps.ToDouble());
 
-		while (!threadShouldExit()) {
-			// Get the start time (to track how long a frame takes to render)
-			const auto time1 = std::chrono::high_resolution_clock::now();
+        while (!threadShouldExit()) {
+            // Get the start time (to track how long a frame takes to render)
+            const auto time1 = std::chrono::high_resolution_clock::now();
 
-			// Get the current video frame (if it's different)
-			frame = getFrame();
+            // Get the current video frame (if it's different)
+            frame = getFrame();
 
-			// Experimental Pausing Code (if frame has not changed)
-			if ((speed == 0 && video_position == last_video_position)
+            // Experimental Pausing Code (if frame has not changed)
+            if ((speed == 0 && video_position == last_video_position)
                 || (video_position > reader->info.video_length)
-            ) {
-				speed = 0;
-				std::this_thread::sleep_for(frame_duration);
-				continue;
-			}
+               ) {
+                speed = 0;
+                std::this_thread::sleep_for(frame_duration);
+                continue;
+            }
 
-			// Set the video frame on the video thread and render frame
-			videoPlayback->frame = frame;
-			videoPlayback->render.signal();
+            // Set the video frame on the video thread and render frame
+            videoPlayback->frame = frame;
+            videoPlayback->render.signal();
 
-			// Keep track of the last displayed frame
-			last_video_position = video_position;
+            // Keep track of the last displayed frame
+            last_video_position = video_position;
 
-			// How many frames ahead or behind is the video thread?
-			int64_t video_frame_diff = 0;
-			if (reader->info.has_audio && reader->info.has_video) {
-				if (speed != 1)
-					// Set audio frame again (since we are not in normal speed, and not paused)
-					audioPlayback->Seek(video_position);
+            // How many frames ahead or behind is the video thread?
+            int64_t video_frame_diff = 0;
+            if (reader->info.has_audio && reader->info.has_video) {
+                if (speed != 1)
+                    // Set audio frame again (since we are not in normal speed, and not paused)
+                    audioPlayback->Seek(video_position);
 
-				// Only calculate this if a reader contains both an audio and video thread
-				audio_position = audioPlayback->getCurrentFramePosition();
-				video_frame_diff = video_position - audio_position;
-			}
+                // Only calculate this if a reader contains both an audio and video thread
+                audio_position = audioPlayback->getCurrentFramePosition();
+                video_frame_diff = video_position - audio_position;
+            }
 
             // Get the end time (to track how long a frame takes to render)
             const auto time2 = std::chrono::high_resolution_clock::now();
@@ -122,8 +122,8 @@ namespace openshot
             // Calculate the amount of time to sleep (by subtracting the render time)
             auto sleep_time = duration_cast<ms>(frame_duration - render_time);
 
-			// Debug
-			ZmqLogger::Instance()->AppendDebugMethod("PlayerPrivate::run (determine sleep)", "video_frame_diff", video_frame_diff, "video_position", video_position, "audio_position", audio_position, "speed", speed, "render_time(ms)", render_time.count(), "sleep_time(ms)", sleep_time.count());
+            // Debug
+            ZmqLogger::Instance()->AppendDebugMethod("PlayerPrivate::run (determine sleep)", "video_frame_diff", video_frame_diff, "video_position", video_position, "audio_position", audio_position, "speed", speed, "render_time(ms)", render_time.count(), "sleep_time(ms)", sleep_time.count());
 
             // Adjust drift (if more than a few frames off between audio and video)
             if (video_frame_diff > 0 && reader->info.has_audio && reader->info.has_video) {
@@ -137,40 +137,40 @@ namespace openshot
                 sleep_time += duration_cast<ms>(video_frame_diff * frame_duration);
             }
 
-			else if (video_frame_diff < -10 && reader->info.has_audio && reader->info.has_video) {
-				// Skip frame(s) to catch up to the audio (if more than 10 frames behind)
-				video_position += std::fabs(video_frame_diff) / 2; // Seek forward 1/2 the difference
-				sleep_time = sleep_time.zero(); // Don't sleep now... immediately go to next position
-			}
+            else if (video_frame_diff < -10 && reader->info.has_audio && reader->info.has_video) {
+                // Skip frame(s) to catch up to the audio (if more than 10 frames behind)
+                video_position += std::fabs(video_frame_diff) / 2; // Seek forward 1/2 the difference
+                sleep_time = sleep_time.zero(); // Don't sleep now... immediately go to next position
+            }
 
             // Sleep (leaving the video frame on the screen for the correct amount of time)
             if (sleep_time > sleep_time.zero()) {
                 std::this_thread::sleep_for(sleep_time);
             }
 
-		}
+        }
     }
 
     // Get the next displayed frame (based on speed and direction)
     std::shared_ptr<openshot::Frame> PlayerPrivate::getFrame()
     {
-	try {
-		// Get the next frame (based on speed)
-		if (video_position + speed >= 1 && video_position + speed <= reader->info.video_length)
-			video_position = video_position + speed;
+    try {
+        // Get the next frame (based on speed)
+        if (video_position + speed >= 1 && video_position + speed <= reader->info.video_length)
+            video_position = video_position + speed;
 
-		if (frame && frame->number == video_position && video_position == last_video_position) {
-			// return cached frame
-			return frame;
-		}
-		else
-		{
-			// Update cache on which frame was retrieved
-			videoCache->setCurrentFramePosition(video_position);
+        if (frame && frame->number == video_position && video_position == last_video_position) {
+            // return cached frame
+            return frame;
+        }
+        else
+        {
+            // Update cache on which frame was retrieved
+            videoCache->setCurrentFramePosition(video_position);
 
-			// return frame from reader
-			return reader->GetFrame(video_position);
-		}
+            // return frame from reader
+            return reader->GetFrame(video_position);
+        }
 
     } catch (const ReaderClosed & e) {
         // ...
