43d25346d66fda291ccc95a8ee8120d75fd5dafc
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-11-26 23:56:43 -0500

    Settings: Add GetSettings(), LIBOPENSHOT_DEBUG
    - Set the environment variable LIBOPENSHOT_DEBUG to any value, to
      activate the DEBUG_TO_STDERR setting.
    - GetSettings() is a shortcut for openshot::Settings::Instance(), e.g.
        name = GetSettings()->PLAYBACK_AUDIO_DEVICE_NAME;

--- src/Settings.h
+++ src/Settings.h
@@ -44,6 +44,7 @@
 #include <unistd.h>
 #include <OpenShotAudio.h>
 
+#define GetSettings() openshot::Settings::Instance()
 
 namespace openshot {
 
@@ -118,7 +119,7 @@ namespace openshot {
 		/// The current install path of OpenShot (needs to be set when using Timeline(path), since certain
 		/// paths depend on the location of OpenShot transitions and files)
 		std::string PATH_OPENSHOT_INSTALL = "";
-    
+
  		/// Whether to dump ZeroMQ debug messages to stderr
 		bool DEBUG_TO_STDERR = false;
 
