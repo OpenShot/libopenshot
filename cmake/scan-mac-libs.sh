0e1822c47fb727eb81f673e0bd6b8efe6b8d582d
FeRD (Frank Dana) <ferdnyc@gmail.com>
2021-09-30 02:07:07 -0400

    Reformat, expand scan script output

--- cmake/scan-mac-libs.sh
+++ cmake/scan-mac-libs.sh
@@ -2,21 +2,31 @@
 # Run otool to display some useful data about the structure of our
 # compiled macOS binary components
 
+function indent() {
+  w=$1
+  ind=$(printf "%${w}s" "")
+  sed -e "s/^/${ind}/;"
+}
+
 cd "${INSTALL_PATH}"
-for lib in libopenshot-audio.dylib libopenshot.dylib libopenshot_protobuf.dylib; do
+for lib in lib/libopenshot-audio.dylib lib/libopenshot.dylib lib/libopenshot_protobuf.dylib python/_openshot.so ; do
+    libname=`basename ${lib}`
     echo
-    echo "== ${lib} =============================="
-    otool -D "lib/${lib}"
-    otool -L "lib/${lib}"
+    echo "== ${libname} =============================="
+    echo -n "${libname} name: "
+    otool -D "${lib}"
+    echo "${libname} libraries:"
+    otool -L "${lib}" | indent 2
     echo
-    echo "${lib} load commands:"
-    otool -l "lib/${lib}"
+    echo "${libname} load commands:"
+    otool -l "${lib}" | indent 2
 done
 echo
 echo "== openshot-player ===================================="
-otool -L "bin/openshot-player"
+echo "openshot-player libraries:"
+otool -L "bin/openshot-player" | indent 2
 echo
 echo "openshot-player load commands:"
-otool -l "bin/openshot-player"
+otool -l "bin/openshot-player" | indent 2
 
 exit 0
