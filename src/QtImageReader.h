dae4b5657ef03243fb423e41a60aca5bc5574b22
FeRD (Frank Dana) <ferdnyc@gmail.com>
2022-01-14 09:14:04 -0500

    QtImageReader.h: Fix indents

--- src/QtImageReader.h
+++ src/QtImageReader.h
@@ -38,110 +38,112 @@
 
 namespace openshot
 {
-    // Forward decl
-    class CacheBase;
-    class Frame;
 
-	/**
-	 * @brief This class uses the Qt library, to open image files, and return
-	 * openshot::Frame objects containing the image.
-	 *
-	 * @code
-	 * // Create a reader for a video
-	 * QtImageReader r("MyAwesomeImage.jpeg");
-	 * r.Open(); // Open the reader
-	 *
-	 * // Get frame number 1 from the video
-	 * std::shared_ptr<Frame> f = r.GetFrame(1);
-	 *
-	 * // Now that we have an openshot::Frame object, lets have some fun!
-	 * f->Display(); // Display the frame on the screen
-	 *
-	 * // Close the reader
-	 * r.Close();
-	 * @endcode
-	 */
-	class QtImageReader : public ReaderBase
-	{
-	private:
-		QString path;
-		std::shared_ptr<QImage> image;			///> Original image (full quality)
-		std::shared_ptr<QImage> cached_image;	///> Scaled for performance
-		bool is_open;	///> Is Reader opened
-		QSize max_size;	///> Current max_size as calculated with Clip properties
+// Forward decl
+class CacheBase;
+class Frame;
+
+/**
+ * @brief This class uses the Qt library, to open image files, and return
+ * openshot::Frame objects containing the image.
+ *
+ * @code
+ * // Create a reader for a video
+ * QtImageReader r("MyAwesomeImage.jpeg");
+ * r.Open(); // Open the reader
+ *
+ * // Get frame number 1 from the video
+ * std::shared_ptr<Frame> f = r.GetFrame(1);
+ *
+ * // Now that we have an openshot::Frame object, lets have some fun!
+ * f->Display(); // Display the frame on the screen
+ *
+ * // Close the reader
+ * r.Close();
+ * @endcode
+ */
+class QtImageReader : public ReaderBase
+{
+
+private:
+	QString path;
+	std::shared_ptr<QImage> image;			///> Original image (full quality)
+	std::shared_ptr<QImage> cached_image;	///> Scaled for performance
+	bool is_open;	///> Is Reader opened
+	QSize max_size;	///> Current max_size as calculated with Clip properties
 
 #if RESVG_VERSION_MIN(0, 11)
-        ResvgOptions resvg_options;
+	ResvgOptions resvg_options;
 #endif
 
-		/// Load an SVG file with Resvg or fallback with Qt
-        ///
-        /// @returns Success as a boolean
-        /// @param path The file path of the SVG file
-        QSize load_svg_path(QString path);
+	/// Load an SVG file with Resvg or fallback with Qt
+	///
+	/// @returns Success as a boolean
+	/// @param path The file path of the SVG file
+	QSize load_svg_path(QString path);
 
-		/// Calculate the max_size QSize, based on parent timeline and parent clip settings
-		QSize calculate_max_size();
+	/// Calculate the max_size QSize, based on parent timeline and parent clip settings
+	QSize calculate_max_size();
 
-		/// Cycle the reader through the open/closed states
-		///
-		/// This will cause it to re-read the image file from disk
-		/// (if a \a path was set), and re-compute metadata properties
-        ///
-        /// @param leave_open Whether the final state should be open or closed
-		void Reload(bool leave_open);
+	/// Cycle the reader through the open/closed states
+	///
+	/// This will cause it to re-read the image file from disk
+	/// (if a \a path was set), and re-compute metadata properties
+	///
+	/// @param leave_open Whether the final state should be open or closed
+	void Reload(bool leave_open);
 
-	public:
-		/// @brief Constructor for QtImageReader.
-		///
-		/// Opens the media file to inspect its properties and loads frame 1,
-		/// iff \a inspect_reader == true (the default). Pass a false value in
-		/// the optional parameter to defer this initial Open()/Close() cycle.
-		///
-		/// When not inspecting the media file, it's much faster, and useful
-		/// when you are inflating the object using JSON after instantiation.
-		QtImageReader(std::string path, bool inspect_reader=true);
+public:
+	/// @brief Constructor for QtImageReader.
+	///
+	/// Opens the media file to inspect its properties and loads frame 1,
+	/// iff \a inspect_reader == true (the default). Pass a false value in
+	/// the optional parameter to defer this initial Open()/Close() cycle.
+	///
+	/// When not inspecting the media file, it's much faster, and useful
+	/// when you are inflating the object using JSON after instantiation.
+	QtImageReader(std::string path, bool inspect_reader=true);
 
-		/// Constructor which takes an existing QImage
-		explicit QtImageReader(const QImage&, bool inspect=true);
+	/// Constructor which takes an existing QImage
+	explicit QtImageReader(const QImage&, bool inspect=true);
 
-		virtual ~QtImageReader() = default;
+	virtual ~QtImageReader() = default;
 
-		/// Close File
-		void Close() override;
+	/// Close File
+	void Close() override;
 
-		/// Get the cache object used by this reader (always returns NULL for this object)
-		CacheBase* GetCache() override { return NULL; };
+	/// Get the cache object used by this reader (always returns NULL for this object)
+	CacheBase* GetCache() override { return NULL; };
 
-		/// Get an openshot::Frame object for a specific frame number of this reader.  All numbers
-		/// return the same Frame, since they all share the same image data.
-		///
-		/// @returns The requested frame (containing the image)
-		/// @param requested_frame The frame number that is requested.
-		std::shared_ptr<Frame> GetFrame(int64_t requested_frame) override;
+	/// Get an openshot::Frame object for a specific frame number of this reader.  All numbers
+	/// return the same Frame, since they all share the same image data.
+	///
+	/// @returns The requested frame (containing the image)
+	/// @param requested_frame The frame number that is requested.
+	std::shared_ptr<Frame> GetFrame(int64_t requested_frame) override;
 
-		/// Provide a new image file path to the Reader
-		void SetPath(const std::string& new_path);
+	/// Provide a new image file path to the Reader
+	void SetPath(const std::string& new_path);
 
-		/// Supply (or replace) the QImage used as the image source
-		void SetQImage(const QImage& new_image);
+	/// Supply (or replace) the QImage used as the image source
+	void SetQImage(const QImage& new_image);
 
-		/// Determine if reader is open or closed
-		bool IsOpen() override { return is_open; };
+	/// Determine if reader is open or closed
+	bool IsOpen() override { return is_open; };
 
-		/// Return the type name of the class
-		std::string Name() override { return "QtImageReader"; };
+	/// Return the type name of the class
+	std::string Name() override { return "QtImageReader"; };
 
-		// Get and Set JSON methods
-		std::string Json() const override; ///< Generate JSON string of this object
-		void SetJson(const std::string value) override; ///< Load JSON string into this object
-		Json::Value JsonValue() const override; ///< Generate Json::Value for this object
-		void SetJsonValue(const Json::Value root) override; ///< Load Json::Value into this object
+	// Get and Set JSON methods
+	std::string Json() const override; ///< Generate JSON string of this object
+	void SetJson(const std::string value) override; ///< Load JSON string into this object
+	Json::Value JsonValue() const override; ///< Generate Json::Value for this object
+	void SetJsonValue(const Json::Value root) override; ///< Load Json::Value into this object
 
-		/// Open File - which is called by the constructor automatically
-		void Open() override;
-	};
+	/// Open File - which is called by the constructor automatically
+	void Open() override;
+};
 
-}
+}  // namespace openshot
 
-#endif
+#endif  // OPENSHOT_QTIMAGE_READER_H
