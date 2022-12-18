39722d0b6c7b78b12de00f68cdbed99d4d343679
Jonathan Thomas <jonathan@openshot.org>
2022-12-18 00:11:34 -0600

    Ignore audio device unit tests on systems with "No Driver" returned in the audio error message

--- tests/AudioDeviceManager.cpp
+++ tests/AudioDeviceManager.cpp
@@ -24,29 +24,32 @@ TEST_CASE( "Initialize Audio Device Manager Singleton", "[libopenshot][AudioDevi
 
     // Invalid sample rate
     AudioDeviceManagerSingleton *mng = AudioDeviceManagerSingleton::Instance(12300, 2);
-    CHECK((mng->defaultSampleRate == 48000 || mng->defaultSampleRate == 44100)); // verify common rate is returned
-    mng->CloseAudioDevice();
+    if (mng->initialise_error.find("No Driver") == std::string::npos) {
+        // Ignore systems with "No Driver" returned in the audio device error
+        CHECK((mng->defaultSampleRate == 48000 || mng->defaultSampleRate == 44100)); // verify common rate is returned
+        mng->CloseAudioDevice();
 
-    // Valid sample rate
-    mng = AudioDeviceManagerSingleton::Instance(44100, 2);
-    CHECK(mng->defaultSampleRate == 44100);
-    mng->CloseAudioDevice();
+        // Valid sample rate
+        mng = AudioDeviceManagerSingleton::Instance(44100, 2);
+        CHECK(mng->defaultSampleRate == 44100);
+        mng->CloseAudioDevice();
 
-    // Valid device type (for Linux)
-    Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE = "ALSA";
-    Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME = "Playback/recording through the PulseAudio sound server";
-    mng = AudioDeviceManagerSingleton::Instance(44100, 2);
-    if (mng->currentAudioDevice.get_name() == Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME &&
-        mng->currentAudioDevice.get_type() == Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE) {
-        // Only check this device if it exists (i.e. we are on Linux with ALSA and PulseAudio)
+        // Valid device type (for Linux)
+        Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE = "ALSA";
+        Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME = "Playback/recording through the PulseAudio sound server";
+        mng = AudioDeviceManagerSingleton::Instance(44100, 2);
+        if (mng->currentAudioDevice.get_name() == Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME &&
+            mng->currentAudioDevice.get_type() == Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE) {
+            // Only check this device if it exists (i.e. we are on Linux with ALSA and PulseAudio)
+            CHECK(mng->defaultSampleRate == 44100);
+            mng->CloseAudioDevice();
+        }
+
+        // Invalid device type (for Linux)
+        Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE = "Fake Type";
+        Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME = "Fake Device";
+        mng = AudioDeviceManagerSingleton::Instance(44100, 2);
         CHECK(mng->defaultSampleRate == 44100);
         mng->CloseAudioDevice();
     }
-
-    // Invalid device type (for Linux)
-    Settings::Instance()->PLAYBACK_AUDIO_DEVICE_TYPE = "Fake Type";
-    Settings::Instance()->PLAYBACK_AUDIO_DEVICE_NAME = "Fake Device";
-    mng = AudioDeviceManagerSingleton::Instance(44100, 2);
-    CHECK(mng->defaultSampleRate == 44100);
-    mng->CloseAudioDevice();
 }
