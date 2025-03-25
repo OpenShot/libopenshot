95f6a6808d60665cfca1641d0ce1f37986efb1f7
HaiVQ <me@haivq.com>
2025-03-25 06:50:56 +0000

    updating blur_radius max value to a more reasonable number

--- src/effects/Shadow.cpp
+++ src/effects/Shadow.cpp
@@ -186,7 +186,7 @@ std::string Shadow::PropertiesJSON(int64_t requested_frame) const {
 	// Keyframes
 	root["x_offset"] = add_property_json("X Offset", x_offset.GetValue(requested_frame), "int", "", &x_offset, -1000, 1000, false, requested_frame);
 	root["y_offset"] = add_property_json("Y Offset", y_offset.GetValue(requested_frame), "int", "", &y_offset, -1000, 1000, false, requested_frame);
-	root["blur_radius"] = add_property_json("Blur Radius", blur_radius.GetValue(requested_frame), "int", "", &blur_radius, 0, 1000, false, requested_frame);
+	root["blur_radius"] = add_property_json("Blur Radius", blur_radius.GetValue(requested_frame), "int", "", &blur_radius, 0, 100, false, requested_frame);
 	root["color"] = add_property_json("Key Color", 0.0, "color", "", &color.red, 0, 255, false, requested_frame);
 	root["color"]["red"] = add_property_json("Red", color.red.GetValue(requested_frame), "float", "", &color.red, 0, 255, false, requested_frame);
 	root["color"]["green"] = add_property_json("Green", color.green.GetValue(requested_frame), "float", "", &color.green, 0, 255, false, requested_frame);
